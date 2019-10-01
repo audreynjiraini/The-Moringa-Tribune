196b2e6c5fc700d469f86a726b9d5df454eadc86
Audrey Wanjiku <audreynjiraini@gmail.com>
2019-10-01 11:44:12 +0300

    uploading images

--- news/urls.py
+++ news/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 # list of url instances for our app. create url instances by calling the url function and pass in the URL regular expression, the view and a name keyword argument.
@@ -8,3 +10,6 @@ urlpatterns=[
     url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_news,name = 'pastNews'),
     url(r'^search/$', views.search_results, name = 'search_results'),
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
