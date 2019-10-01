8aa49d044ed8145fe4b03337413e9cfcf499b4dd
Audrey Wanjiku <audreynjiraini@gmail.com>
2019-10-01 10:41:46 +0300

    updating views

--- news/views.py
+++ news/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404 # responsible for returning a response to a user
 import datetime as dt
+from .models import Article
 
 
 # Create your views here.
@@ -9,10 +10,17 @@ def welcome(request):
     return render(request, 'welcome.html')
 
 
-def news_of_day(request):
-    date = dt.date.today()
+# def news_of_day(request):
+#     date = dt.date.today()
     
-    return render(request, 'all-news/today-news.html', {"date": date,})
+#     return render(request, 'all-news/today-news.html', {"date": date,})
+
+
+def news_today(request):
+    date = dt.date.today()
+    news = Article.today_news()
+    
+    return render(request, 'all-news/today-news.html', {"date": date, "news": news})
 
 def convert_dates(dates):
     # Function that gets the weekday number for the date.
@@ -24,6 +32,25 @@ def convert_dates(dates):
     # Returning the actual day of the week
     day = days[day_number]
     return  day
+
+
+# # View Function to present news from past days
+# def past_days_news(request, past_date):
+    
+#     try:
+        
+#         # Converts data from the string URL
+#         date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
+        
+#     except ValueError:
+#         # Raise 404 error when ValueError is thrown
+#         raise Http404()
+#         assert False
+        
+#     if date == dt.date.today():
+#         return redirect(news_of_day)
+    
+#     return render(request, 'all-news/past-news.html', {"date": date})
 
 
 # View Function to present news from past days
@@ -40,6 +67,8 @@ def past_days_news(request, past_date):
         assert False
         
     if date == dt.date.today():
-        return redirect(news_of_day)
+        return redirect(news_today)
     
-    return render(request, 'all-news/past-news.html', {"date": date})
+    news = Article.days_news(date)
+    
+    return render(request, 'all-news/past-news.html', {"date": date, "news": news})
