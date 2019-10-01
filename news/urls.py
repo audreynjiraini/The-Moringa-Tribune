7f0ee55a5e5cafd443427ac7a990c09c3c48d767
Audrey Wanjiku <audreynjiraini@gmail.com>
2019-10-01 09:58:30 +0300

    adding models to the admin page

--- news/urls.py
+++ news/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 # list of url instances for our app. create url instances by calling the url function and pass in the URL regular expression, the view and a name keyword argument.
 urlpatterns=[
-    url('^$', views.welcome, name= 'welcome'),
-    url('^today/$', views.news_of_day, name= 'newsToday'),
+    url('^$', views.news_of_day, name= 'newsToday'),
     url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_news,name = 'pastNews')
 ]
